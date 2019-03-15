acc0d0e3fb48c8aa29dfabbf708c9f10a18ebe72
Mathis Dankou <math98dankou@gmail.com>
2019-03-15 18:30:12 +0100

    edit Piece

---
+++ src/Piece.java
@@ -0,0 +1,23 @@
+/**
+ * Décrivez votre classe Client ici.
+ *
+ * @author Mathis Dankou
+ * @version 15/03/2019
+ */
+
+public abstract class Piece
+{
+	private int color;
+	
+	/**
+	 * Cette méthode retourne la couleur de la piece
+	 * @return	int la couleur de la piece 
+	 */
+	public int getColor(){return this.color;}
+	
+	/**
+	 * Cette méthode permet à la piece de se déplacer
+	 */
+	public abstract void move();
+	
+}
