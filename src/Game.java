cfdb2386c763f73c5673b1e8661642e8612055d3
PetitEsprit <PetitEsprit@users.noreply.github.com>
2019-03-21 12:31:43 +0100

    some func in Game.java

--- src/Game.java
+++ src/Game.java
@@ -20,14 +20,36 @@ public class Game
 	}
 	
 	/**
+	 *Vérifie si une coordonnée est dans les limites du plateau
+	 */
+	private boolean isOnBoard()
+	{
+		if(/*Coor audelà des limites*/)return false;
+		return true;
+	}
+	
+	/**
+	 *Interprète les commande envoyé par un des joueurs  
+	 **/
+	private void parseCom(String )
+	{
+		
+	}
+	 
+	/**
 	 * S'occupe de la logique de jeu
 	 */
 	public void run()
 	{
 		
-		//while(/*Un des rois en situation d'échecs*/)
+		while(!isCheckmate())
 		{
-			
+			/*
+			 * - 1.Attente Entrée joueur n
+			 * - 2.Vérif des entrées
+			 * - 3.Affichage 
+			 * - 4.changement de tour
+			 * */
 		}
 	}
 
