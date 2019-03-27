3b68f0b380f31351238f753c3ccc10162808cca5
Utilisateur <user@debian.arcahe.ovh>
2019-03-27 16:10:26 +0100

    chocolatine

---
+++ src/Rook.java
@@ -0,0 +1,108 @@
+/**
+ * Classe de la tour(Exemple)
+ *
+ * @author	Franck Naze
+ * @version	16/03/2019
+ */
+
+public class Rook extends Piece
+{
+	/**
+	 * Cette méthode permet à la piece de se déplacer (que j' @Override)
+	 * @param	x1 la position x de départ de la pièce
+	 * @param	y1 la position y de départ de la pièce
+	 * @param	x2 la position x de d'arrivé de la pièce
+	 * @param	y2 la position y de d'arrivé de la pièce
+	 * @return	un boolean indiquant si le mouvement est possible ou non
+	 */
+	 
+		public Rook(int color)
+	{
+		// Affectation des sprites
+		super(color,"♖","♜");
+	}
+	/**
+	 * Cette méthode permet à la piece de se déplacer (que j' @Override)
+	 * @param	x1 la position x de départ de la pièce
+	 * @param	y1 la position y de départ de la pièce
+	 * @param	x2 la position x de d'arrivé de la pièce
+	 * @param	y2 la position y de d'arrivé de la pièce
+	 * @return	un boolean indiquant si le mouvement est possible ou non
+	 */ 
+	 @Override
+	public boolean movePossible(ChessBoard b, int x1, int y1, int x2, int y2)
+	{
+		int dx;
+		int dy;
+	    if(x1==x2 && y1==y2) return false;//deplacement nul bloquée
+		if (x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0)//hors limites
+			return false; 
+		else if (x1 > 7 || x2 > 7 || y1 > 7 || y2 > 7)//hors limites
+			return false;
+			
+		dx=Math.abs(x2 - x1);
+		dy=Math.abs(y2 - y1);
+		
+		if(dx>0 && dy>0) return false;//deplacement vertical et horizontal à la fois bloquée 
+		
+			if(dx!=0)
+			{
+				while(x1 != x2)
+				{
+				    if(x1<x2)
+				    {
+						x1+=1;
+						if((x1==x2))
+						{
+						if (b.isEmpty(x2, y2) == false && b.getPiece(x2, y2).getColor() == this.getColor()) return false;
+						else
+						return true;
+					    }
+					    if(b.isEmpty(x1,y1)==false)return false;
+					}
+					if(x1>x2)
+					{
+						x1-=1;
+						if(x1==x2)
+						{
+						if (b.isEmpty(x2, y2) == false && b.getPiece(x2, y2).getColor() == this.getColor()) return false;
+						else
+						return true;
+					    }
+					    if(b.isEmpty(x1,y1)==false)return false;
+					}
+				  }
+			}
+			if(dy!=0)
+			{
+				while(y1 != y2)
+				{
+					if(y1<y2)
+					{
+					 y1+=1;
+						if((y1==y2))
+						{
+						if (b.isEmpty(x2, y2) == false && b.getPiece(x2, y2).getColor() == this.getColor()) return false;
+						else
+						return true;
+					    }
+					    if(b.isEmpty(x1,y1)==false)return false;
+					}
+					else
+					{
+						y1-=1;
+						if((y1==y2))
+						{
+						if (b.isEmpty(x2, y2) == false && b.getPiece(x2, y2).getColor() == this.getColor()) return false;
+						else
+					    return true;
+					    }
+					    if(b.isEmpty(x1,y1)==false)return false;
+					}
+				}
+			}	
+		
+		return false;
+	}
+	
+}	
